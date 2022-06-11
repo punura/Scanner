9d0c427eed9e7be9ab8fd9dbd4c59f81704bc224
Punura Wathsara <123pemmawadupunura@gmail.com>
2022-06-11 14:17:39 +0530

    Changed bin/Main.class

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,15 @@ public class Main {
 		String food = scanner.nextLine();
 		System.out.println("What is your grade? ");
 		String grade = scanner.nextLine();
+		System.out.println("What is your favourite color?");
+		String color = scanner.nextLine();
 		
 		
 		System.out.println("Hello "+name);
 		System.out.println("You are "+age+" years old");
 		System.out.println("You like "+food);
 		System.out.println("Your grade is "+grade);
+		System.out.println("Your favourite color is "+color);
 		
 		scanner.close();
 
